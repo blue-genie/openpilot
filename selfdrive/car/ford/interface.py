efb4a5966a74a8629dd1ad5da37d2346295568a7
rwxr-xr-x <rwxr-xr-x>
2023-10-06 12:33:41 -0500

    Enable radar only in F150

--- selfdrive/car/ford/interface.py
+++ selfdrive/car/ford/interface.py
@@ -16,7 +16,7 @@ class CarInterface(CarInterfaceBase):
     ret.carName = "ford"
     # ret.dashcamOnly = candidate in {CAR.F_150_MK14}
 
-    ret.radarUnavailable = False
+    ret.radarUnavailable = candidate not in {CAR.F_150_MK14}
     ret.steerControlType = car.CarParams.SteerControlType.angle
     ret.steerActuatorDelay = 0.2
     ret.steerLimitTimer = 1.0
