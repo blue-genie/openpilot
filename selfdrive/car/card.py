fe2ae897002a326025fb2b68bfee162f1134faa9
blue-genie <8130363+blue-genie@users.noreply.github.com>
2024-12-03 08:25:52 -0600

    Enable F150

--- selfdrive/car/card.py
+++ selfdrive/car/card.py
@@ -115,6 +115,7 @@ class Car:
 
     openpilot_enabled_toggle = self.params.get_bool("OpenpilotEnabledToggle")
 
+    self.CP.dashcamOnly = False
     controller_available = self.CI.CC is not None and openpilot_enabled_toggle and not self.CP.dashcamOnly
 
     self.CP.passive = not controller_available or self.CP.dashcamOnly
